dfa630fc27620fd9d57fd803c1ab58f266137e6a
Polar Humenn <polar@adiron.com>
2011-03-21 15:05:07 -0400

    Forgot coordinates in Location. Requires reload.

--- db/migrate/20110318204443_create_locations.rb
+++ db/migrate/20110318204443_create_locations.rb
@@ -3,6 +3,7 @@ class CreateLocations < ActiveRecord::Migration
     create_table :locations do |t|
       t.string   "name"
       t.string   "description", :default => ""
+      t.text     "coordinates"
 
       t.timestamps
     end
